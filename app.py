d95a700d9c2ae0c854ac6bc72063b26f571661cb
xaviersvd <isac.lx90@gmail.com>
2026-02-18 13:30:39 -0300

    Adiciona objetos ao cardapio

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Gui', 10)
-restaurante_praca.receber_avaliacao('Lais', 8)
-restaurante_praca.receber_avaliacao('Emy', 2)
+bebida_suco = Bebida('Suco de Melancia', 5.00, 'Grande')
+
+prato_camarao = Prato('Massa de Camarão', 45.00, 'A melhor massa da cidade de Natal.')
+
 
 def main():
-    Restaurante.listar_restaurantes()
+    print(bebida_suco)
+    print(prato_camarao)
 
 if __name__ == '__main__':
     main()
