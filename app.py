31c6f4118ce91a3cb2cd0be73037f050ed0242b3
xaviersvd <isac.lx90@gmail.com>
2026-02-18 14:01:12 -0300

    adicona funcao para adicionar itens ao cardapio

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from modelos.cardapio.bebida import Bebida
 from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-bebida_suco = Bebida('Suco de Melancia', 5.00, 'Grande')
 
+bebida_suco = Bebida('Suco de Melancia', 5.00, 'Grande')
 prato_camarao = Prato('Massa de Camarão', 45.00, 'A melhor massa da cidade de Natal.')
-restaurante_praca.adciona_prato_no_cardapio(bebida_suco)
-restaurante_praca.adciona_prato_no_cardapio(prato_camarao)
+
+restaurante_praca.adiciona_no_cardapio(bebida_suco)
+restaurante_praca.adiciona_no_cardapio(prato_camarao)
 
 
 def main():
